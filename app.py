139b3c1dc31a5fd50f62b81f0167877b914421b9
mattpodolak <mpodola2@gmail.com>
2018-07-27 19:35:49 -0400

    starter code for pygame

---
+++ app.py
@@ -0,0 +1,10 @@
+import sys, pygame
+
+#pylint shows as an errror
+pygame.init()
+
+# window size
+size = width, height = 320, 240
+
+# Create graphical window
+screen = pygame.display.set_mode(size)
