340eb99618615e1371ae5628b3c23a70f49cb291
mattpodolak <mpodola2@gmail.com>
2018-07-27 21:39:53 -0400

    making room generation

--- map_gen.py
+++ map_gen.py
@@ -13,38 +13,55 @@ def generateMap(min_width, max_width, min_height, max_height):
     # create array of 1s, dimensions equal map size (1 represents wall, 0 is floor, 2 is start, 3 is goal)
     return mapArr = np.ones((mapWidth, mapHeight), dtype=int)
 
-def generateRooms(min_rooms, max_rooms, min_size, max_size, min_dist):
+def generateRooms(min_rooms, max_rooms, min_size, max_size, min_dist, mapWidth, mapHeight):
     
     # pick number of rooms
     print('Making map..')
+    num_rooms = random.randint(min_rooms, max_rooms+1)
 
-def Room(room_x, room_y, maxWidth, maxHeight, minmax):
-    # plus or minus minmax 
-    plus_x =   random.randint(0, minmax+1)
-    minus_x =   random.randint(0, minmax+1)
-    plus_y =   random.randint(0, minmax+1)
-    minus_y =   random.randint(0, minmax+1)
+    # store room info
+    room_data = []
 
-    # make sure room doesn't go off the map
-    # calculate x room variables
-    if(room_x + plus_x > maxWidth-1):
-        room_x2 = maxWidth - 1
-    else:
-        room_x2 = room_x + plus_x
-    
-    if(room_x - minus_x < 0):
-        room_x1 = 0
-    else:
-        room_x1 = room_x - minus_x
+    # generate rooms
+    for i in range(0, num_rooms):
+        print('Making: Room ', i)
+        # pick width and height
+        width = random.randint(min_size, max_size+1)
+        height = random.randint(min_size, max_size+1)
 
-    # calculate y room variables
-    if(room_y + plus_y > maxHeight-1):
-        room_y2 = maxHeight - 1
-    else:
-        room_y2 = room_y + plus_y
-    if(room_y - minus_y < 0):
-        room_y1 = 0
-    else:
-        room_y1 = room_y - minus_y
+        if i != 0:
+            conflict = True
+            loop_count = 0
 
-    return room_x1, room_x2, room_y1, room_y2
+            while conflict == True:
+                conflict = False
+                x_loc = random.randint(0, mapWidth-width)
+                y_loc = random.randint(0, mapHeight-height)
+
+                if loop_count == 5:
+                    print('OH NO! making room smallest size')
+                    width = min_size
+                    height = min_size
+                
+                if loop_count == 10:
+                    print('ITS NOT WORKING, making size 1 room')
+                    width = 1
+                    height = 1
+
+                # check previous locations for conflicts
+                for room in room_data:
+                    if (room['x_loc']+room['width']+min_dist > x_loc || room['y_loc']+room['height']+min_dist > y_loc || x_loc+width+min_dist > room['x_loc'] || y_loc+height+min_dist > room['y_loc']):
+                        print('Room overlap!')
+                        conflict = True
+                        break
+                loop_count++
+
+        else:
+            x_loc = random.randint(0, mapWidth-width)
+            y_loc = random.randint(0, mapHeight-height)
+
+        # pick location with no conflicts, further than min dist away from nearest room
+        # if conflicts make room smaller
+        room_data.append({width})
+
+
